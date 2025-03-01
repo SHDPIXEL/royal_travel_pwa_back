a77eae4d5d168818671a662037634053cf30ea76
SHDPIXEL <shdpixel@gmail.com>
2025-03-01 20:03:53 +0530

    Update usercontroller.js

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -444,7 +444,7 @@ const order = async (req, res) => {
 
     // Razorpay instance setup
     const instance = new Razorpay({
-      key_id: process.env.RAZORPAY_KEY_ID,
+      key_id: process.env.RAZORPAY_KEY_ID_s,
       key_secret: process.env.RAZORPAY_KEY_SECRET,
     });
 
